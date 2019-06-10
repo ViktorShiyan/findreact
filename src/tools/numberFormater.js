b78e02f71b65a754133562945bc82ffc36fdf177
ViktorShiyan <VictorShyan@yandex.ru>
2019-06-10 22:13:19 +0600

    Build project

--- src/tools/numberFormater.js
+++ src/tools/numberFormater.js
@@ -1,9 +1,6 @@
 let numberAddSeven = (number) => {
     const prefix = '+7';
-    let newNumber="";
-    if (number.length === 1) {
-        newNumber = number;
-    } else if (number.length > 1) {
+    let newNumber="";if (number.length > 1) {
         newNumber = number.substring(2);
     }
     return `${prefix}${newNumber}`;
