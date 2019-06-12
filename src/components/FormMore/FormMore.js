f21bd32c0d058676b07600309180d47130f30250
ViktorShiyan <VictorShyan@yandex.ru>
2019-06-12 11:14:35 +0600

    made more find number

--- src/components/FormMore/FormMore.js
+++ src/components/FormMore/FormMore.js
@@ -5,12 +5,12 @@ import formater from '../../tools/numberFormater';
 export default class FormMore extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {text: ""};
+        this.state = {code: []};
     }
 
 
     requestToApi = () => {
-        let url = "https://viktorshiyan.ru/find?number=" + this.state.text.substring(2);
+        let url = "https://viktorshiyan.ru/findmore?number=" + this.state.text;
 
         request({
             url: url,
@@ -18,9 +18,10 @@ export default class FormMore extends React.Component {
         }, (error, response, body) => {
             if ((!error && response.statusCode === 200) || (!error && response.statusCode === 304)) {
                 console.log(body);// Print the json response
-                this.setState({code: body.code});
-                this.setState({company: body.company});
-                this.setState({region: body.region});
+                let d = body.array;
+                console.log('THIS is Array' + JSON.stringify(d));
+                this.setState({code: body.array});
+                console.log(this.state.code[0]);
             }
         })
     };
@@ -41,22 +42,26 @@ export default class FormMore extends React.Component {
                 <div className={"jumbotron"}>
                     <p className={"display-4"}>Информация о списке номеров</p>
                     <div className={"alert alert-primary"}>
-                        <p>Введите номер без семерки.</p>
+                        <p>Для правильной работы приложения номера нужно вводить без семерки (в формате 9879991234).
+                            Что бы получить информацию о нескольких номерах разделяйте номера символом ";"
+                            (пример 9879991234;9879991235;9879991236)</p>
                     </div>
-                    <form className={"form-inline"} onSubmit={this.handleSubmit}>
-                        <div className="form-group mb-2">
-                            <input type="text" readOnly className="form-control-plaintext" id="staticEmail2"
-                                   value="Формат ввода 9871235566"/>
-                        </div>
+                    <form className={"form"} onSubmit={this.handleSubmit}>
                         <div className={"form-group mx-sm-3 mb-2"}>
-                            <input placeholder={"Enter number"} className={"form-control form-control-lg"}
-                                   onChange={this.handleChange} value={formater(this.state.text)}/>
+                            <textarea className={"form-control form-control-lg"}
+                                      onChange={this.handleChange}>{this.state.text}</textarea>
                         </div>
                         <input className={"btn btn-primary mb-2"} type='submit' value='Отправить'/>
                     </form>
-                    <p>{formater(this.state.text)}</p>
-                    <p>{this.state.company}</p>
-                    <p>{this.state.region}</p>
+                    <p>{this.state.code.length !== 0 ? this.state.code.reduce((acc, cur) => {
+                        if (cur) {
+                            return [...acc, `Номер:${cur.number} Регион:${cur.region} Компания:${cur.company}`];
+                        } else {
+                            return [...acc, `Ошибка формата номера. Проверьте правильность номера`];
+                        }
+                    }, []).map((cur) => {
+                        return <p> {cur}</p>
+                    }) : 'Ждём запрос'}</p>
                 </div>
             </main>
         )
