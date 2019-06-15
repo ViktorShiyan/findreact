104bbfbe9196656c0c99b46e064e9190a001820a
ViktorShiyan <VictorShyan@yandex.ru>
2019-06-15 13:04:47 +0600

    add 404 flex

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -4,7 +4,7 @@ const NotFound = () => {
     return (
         <div >
             <p className={'display-4'}>Страница не найдена 404</p>
-            <img src="https://css-tricks.com/wp-content/themes/CSS-Tricks-17/images/ripped.jpg" alt="" />
+            <img src="https://css-tricks.com/wp-content/themes/CSS-Tricks-17/images/ripped.jpg" alt="" className="img-fluid"/>
         </div>
     )
 };
