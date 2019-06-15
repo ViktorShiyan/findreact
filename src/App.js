0924864c280859483f336f22ecb66b7226449c50
ViktorShiyan <VictorShyan@yandex.ru>
2019-06-15 09:05:08 +0600

    add 404 page

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 import Form from './components/Form/Form';
 import Footer from "./components/Footer/Footer";
 import Start from './components/Start/Start'
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import FormMore from "./components/FormMore/FormMore";
+import NotFound from "./components/NotFound";
 
 function App() {
     return (
@@ -12,10 +13,13 @@ function App() {
             <div className="App">
                 <Footer/>
                 <div className={"content"}>
-                    <Route path="//" component={Start}/>
+                    <Switch>
+                    <Route path="//"  component={Start}/>
                     <Route path="/home/" component={Start}/>
                     <Route path="/first/" component={Form}/>
                     <Route path="/more/" component={FormMore}/>
+                    <Route component={NotFound}/>
+                    </Switch>
                 </div>
             </div>
         </Router>
---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div >
+            <p className={'display-4'}>Страница не найдена 404</p>
+            <img src="https://css-tricks.com/wp-content/themes/CSS-Tricks-17/images/ripped.jpg" alt="" />
+        </div>
+    )
+};
+export default NotFound;
